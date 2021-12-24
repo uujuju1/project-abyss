a4539de85dcc36a5b18a6e291f58dd44c4e7e795
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-24 19:27:41 -0300

    Add files via upload

--- src/abyss/content/AbyssBlocks.java
+++ src/abyss/content/AbyssBlocks.java
@@ -14,18 +14,52 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.defense.*;
 import mindustry.world.meta.*;
 import mindustry.world.draw.*;
 
 import abyss.content.*;
+import abyss.world.blocks.defense.*;
 
 import static mindustry.type.ItemStack.with;
 
 public class AbyssBlocks implements ContentList {
-	public Block abyssForge;
+	public static Block 
+		// walls
+		voidWall, voidWallLarge, voidWallHuge,
+		// production 
+		abyssForge;
 
 	@Override
 	public void load() {
+		voidWall = new UnstableWall("void-wall") {{
+			requirements(Category.crafting, with(
+				AbyssItems.abyss, 6
+			));
+			size = 1;
+			health = 830;
+			damage = 120;
+			damageRadius = 40;
+		}};
+		voidWallLarge = new  UnstableWall("void-wall-large") {{
+			requirements(Category.crafting, with(
+				AbyssItems.abyss, 24
+			));
+			size = 2;
+			health = 830 * 4;
+			damage = 300;
+			damageRadius = 80;
+		}};
+		voidWallHuge = new UnstableWall("void-wall-huge") {{
+			requirements(Category.crafting, with(
+				AbyssItems.abyss, 54
+			));
+			size = 3;
+			health 830 * 9;
+			damage = 750;
+			damageRadius = 120;
+		}};
+
 		abyssForge = new GenericCrafter("abyss-forge") {{
 			requirements(Category.crafting, with(
 				Items.titanium, 60,
---
+++ src/abyss/world/blocks/defense/UnstableWall.java
@@ -0,0 +1,24 @@
+package abyss.world.blocks.defense;
+
+import mindustry.gen.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.world.blocks.defense.Wall;
+
+public class UnstableWall extends Wall {
+	public int damage = 69
+	public float damageRadius = 420;
+	public Effect explodeEffect = Fx.none;
+
+	public UnstableWall(String name) {
+		super(name);
+	}
+
+	public class UnstableWallBuild extends WallBuild {
+		@Override
+		public void onDestroyed() {
+			Damage.damage(x, y, damageRadius, damage);
+			explodeEffect.at(x, y);
+		}
+	}
+}
