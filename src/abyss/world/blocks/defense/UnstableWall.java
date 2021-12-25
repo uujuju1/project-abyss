aa6636cb478d5cbdf418b4d5f748572b66d7a893
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-25 00:06:19 -0300

    Add files via upload

--- src/abyss/world/blocks/defense/UnstableWall.java
+++ src/abyss/world/blocks/defense/UnstableWall.java
@@ -1,7 +1,11 @@
 package abyss.world.blocks.defense;
 
+import arc.Core;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
 import mindustry.gen.*;
 import mindustry.content.*;
+import mindustry.graphics.*;
 import mindustry.entities.*;
 import mindustry.world.blocks.defense.Wall;
 
@@ -9,9 +13,18 @@ public class UnstableWall extends Wall {
 	public int damage = 69;
 	public float damageRadius = 420f;
 	public Effect explodeEffect = Fx.none;
+	public Color topColor = Pal.turretHeat;
+
+	public TextureRegion topRegion;
 
 	public UnstableWall(String name) {
 		super(name);
+	}
+
+	@Override
+	public void load() {
+		super.load();
+		topRegion = Core.atlas.find(name + "-top");
 	}
 
 	public class UnstableWallBuild extends WallBuild {
@@ -19,6 +32,16 @@ public class UnstableWall extends Wall {
 		public void onDestroyed() {
 			Damage.damage(this.team, x, y, damageRadius, damage);
 			explodeEffect.at(x, y);
+		}
+
+		@Override
+		public void draw() {
+			float alpha = health/this.health;
+			super.draw();
+			Draw.alpha(alpha);
+			Draw.color(topColor);
+			Draw.rect(topRegion, x, y, 0);
+			Draw.reset();
 		}
 	}
 }
