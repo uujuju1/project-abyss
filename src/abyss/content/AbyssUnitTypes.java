2d086ca117f7d5a12cddbd25fde4e5349b94f9fd
uujuju <84468589+uujuju1@users.noreply.github.com>
2021-12-26 12:32:15 -0300

    Add files via upload

--- src/abyss/content/AbyssUnitTypes.java
+++ src/abyss/content/AbyssUnitTypes.java
@@ -16,9 +16,49 @@ public class AbyssUnitTypes implements ContentList {
 		antra = new UnitType("antra") {{
 			health = 250;
 			speed = 2f;
-			rotateSpeed = 4f;
+			rotateSpeed = 8f;
 			flying = true;
 			constructor = UnitEntity::create;
+			range = 30f;
+			maxRange = range;
+			weapons.add(
+				new Weapon() {{
+					x = y = 0f;
+					reloadTime = 30f;
+					mirror = false;
+					shootSound = Sounds.plasmaBoom;
+					bullet = new BombBulletType(27f, 30f) {{
+						width = height = hitSize * 2f;
+						lifetime = 60f;
+						frontColor = Color.valueOf("2E2E2E");
+						backColor = Color.valueOf("1C1C1C");
+					}};
+				}}
+			);
+		}};
+		karma = new UnitType("karma") {{
+			health = 475;
+			speed = 1.7f;
+			rotateSpeed = 7f;
+			flying = true;
+			constructor = UnitEntity::create;
+			range = 120f;
+			maxRange = range;
+			weapons.add(
+				new Weapon("void-mount") {{
+					x = 2f;
+					y = 0f;
+					reload = 60f;
+					recoil = 1f;
+					bullet = new BasicBulletType(4f 30f) {{
+						width = 9f;
+						height = 11f;
+						lifetime = 30f;
+						frontColor = Color.valueOf("2E2E2E");
+						backColor = Color.valueOf("1C1C1C");
+					}};
+				}}
+			);
 		}};
 	}
 }
