1118d25a78ac8d0553c1078ff83432afb8d68e76
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-04 22:40:28 -0300

    Add files via upload

--- src/abyss/world/blocks/production/ProximityCrafter.java
+++ src/abyss/world/blocks/production/ProximityCrafter.java
@@ -7,6 +7,7 @@ import mindustry.world.meta.*;
 import mindustry.world.blocks.defense.Wall.*;
 import mindustry.world.consumers.*;
 import mindustry.type.*;
+import mindustry.world.meta.*;
 import mindustry.world.Block;
 
 public class ProximityCrafter extends Block {
@@ -22,6 +23,32 @@ public class ProximityCrafter extends Block {
 		sync = true;
 		ambientSoundVolume = 0.03f;
 		flags = EnumSet.of(BlockFlag.factory);
+	}
+
+	@Override
+	public void setStats() {
+		super.setStats();
+		stats.add(Stat.productionTime, craftTime/60f, StatUnit.seconds);
+		if (outputItem != null) {
+			stats.add(Stat.output, StatValues.items(craftTime, outputItem));
+		}
+	}
+	// code repeated since im scared that it cant get build code
+	public float getProximityBlocks() {
+			float mult = 0f;
+			for (int i = 0; i < this.proximity.size; i++) {
+				if (this.proximity.get(i) instanceof WallBuild) {
+					mult += 0.1f;
+				}
+			}
+			return mult;
+		}
+
+	@Override
+	public void setBars() {
+		super.setBars();
+		// this will likely break if a block passes size 10
+		bars.add("Efficiency", e -> new Bar(Core.bundle.format("bar.efficiency"), Pal.power, () -> this.proximity.size/this.getProximityBlocks() * 10));
 	}
 
 	public class ProximityCrafterBuild extends Building {
