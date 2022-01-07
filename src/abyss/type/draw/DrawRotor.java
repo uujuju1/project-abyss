004910c436185a5aceec4037907c3f6ace3ccc54
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-07 11:12:07 -0300

    Add files via upload

--- src/abyss/type/draw/DrawRotor.java
+++ src/abyss/type/draw/DrawRotor.java
@@ -32,11 +32,10 @@ public class DrawRotor {
 	}
 
 	public void draw(Unit unit) {
-		float rx = unit.x + Angles.trnsx(Unit.rotation - 90, x, y);
-		float ry = unit.y + Angles.trnsy(Unit.rotation - 90, x, y);
+		float rx = unit.x + Angles.trnsx(unit.rotation - 90, x, y);
+		float ry = unit.y + Angles.trnsy(unit.rotation - 90, x, y);
 
 		Draw.rect(rotorRegion, rx, ry, rotation + unit.rotation);
 		Draw.rect(jointRegion, rx, ry, 0 + unit.rotation);
-		
 	}
 }
