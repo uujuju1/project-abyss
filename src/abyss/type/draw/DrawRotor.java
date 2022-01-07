4a991e57fa2b9dff7f70012ca6e9cef2ee7e26f2
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-07 11:02:45 -0300

    Add files via upload

---
+++ src/abyss/type/draw/DrawRotor.java
@@ -0,0 +1,42 @@
+package abyss.type.draw;
+
+import arc.Core;
+import arc.util
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.Time;
+import mindustry.gen.Unit;
+
+public class DrawRotor {
+	public final String name;
+	public TextureRegion rotorRegion, jointRegion;
+
+	public float x = 0f, y = 0f, s = 0f;
+
+	float rotation = 0f;
+
+	public DrawRotor(String name) {
+		this.name = name;
+	}
+
+	public void load() {
+		rotorRegion = Core.atlas.find(name);
+		jointRegion = Core.atlas.find(name + "-joint");
+	}
+
+	public void update() {
+		rotation += Time.delta * s;
+		if (rotation > 360f) {
+			rotation -= 360f;
+		}
+	}
+
+	public void draw(Unit unit) {
+		float rx = unit.x + Angles.trnsx(Unit.rotation - 90, x, y);
+		float ry = unit.y + Angles.trnsy(Unit.rotation - 90, x, y);
+
+		Draw.rect(rotorRegion, rx, ry, rotation + unit.rotation);
+		Draw.rect(jointRegion, rx, ry, 0 + unit.rotation);
+		
+	}
+}
