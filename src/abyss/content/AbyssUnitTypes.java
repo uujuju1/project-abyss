8e7ba3a3c1c11872f4752aa1383137ad5e3ff051
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-08 18:37:36 -0300

    Add files via upload

--- src/abyss/content/AbyssUnitTypes.java
+++ src/abyss/content/AbyssUnitTypes.java
@@ -2,6 +2,7 @@ package abyss.content;
 
 import arc.graphics.Color;
 import arc.struct.*;
+import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.entities.bullet.*;
 import mindustry.world.meta.*;
