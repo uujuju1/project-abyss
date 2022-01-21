a837cfcc6d21ee7730cc0be016a017fd935c65e6
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-21 19:02:00 -0300

    Add files via upload

---
+++ src/abyss/ReflectiveAbility.java
@@ -0,0 +1,43 @@
+package abyss.entities.abilities;
+
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.entities.abilities.Ability.*;
+
+public class ReflectiveAbility extends Ability {
+	public float damage, radius;
+	public boolean damagesAir, damagesGround;
+	public Effect damageEffect;
+	float health = 0f;
+
+	public ReflectiveAbility(float damage, float radius, boolean damagesAir, boolean damagesGround, Effect damageEffect) {
+		this.damage = damage;
+		this.radius = radius;
+		this.damagesGround = damagesGround;
+		this.damagesAir = damagesAir;
+		this.damageEffect = damageEffect;
+	}
+	public ReflectiveAbility(float damage, float radius, boolean damagesAir, boolean damagesGround) {
+		this.damage = damage;
+		this.radius = radius;
+		this.damagesGround = damagesGround;
+		this.damagesAir = damagesAir;
+		this.damageEffect = Fx.none;
+	}
+	public ReflectiveAbility(float damage, float radius) {
+		this.damage = damage;
+		this.radius = radius;
+		this.damagesGround = true;
+		this.damagesAir = true;
+		this.damageEffect = Fx.none;
+	}
+
+	@Override
+	public void update(Unit u) {
+		if (health > u.health) {
+			Damage.damage(u.x, u.y, radius, damage, )
+		}
+		health = u.health;
+	}
+}
