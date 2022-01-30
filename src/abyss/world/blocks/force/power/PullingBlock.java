7f8d4c0a41673198e0ebbfc0732795bd397fac5e
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-30 11:11:50 -0300

    Add files via upload

---
+++ src/abyss/world/blocks/force/power/PullingBlock.java
@@ -0,0 +1,31 @@
+package abyss.world.blocks.force.power;
+
+import abyss.world.blocks.force.*;
+
+public class PullingBlock extends ForceBlock {
+	public float pullMagnitude = 1f, releaseMagnitude = 0.2f;
+
+	public PullingBlock(String name, Color forceColor) {
+		super(name, forceColor);
+		maxForce = 100f;
+		configurable = true;
+	}
+
+	public class PullingBlockBuild extends ForceBlock.ForceBlockBuild {
+		boolean willPull = false;
+
+		@Override
+		public void buildConfiguration(Table table) {
+			table.button(Icon.settings, () -> {willPull = true;});
+		}
+
+		@Override
+		public void updateTile() {
+			if (willPull) {
+				setForce(Math.min(force.force + (Time.delta * pullMagnitude), maxForce));
+			} else {
+				setForce(Math.max(force.force - (Time.delta * releaseMagnitude), 0f));
+			}
+		}
+	}
+}
