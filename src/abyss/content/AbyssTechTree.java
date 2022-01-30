77086dc59935de4f679f0ccd7109716a76dee26e
1237 <92003875+12three7@users.noreply.github.com>
2022-01-30 12:27:29 +0800

    Update AbyssTechTree.java

--- src/abyss/content/AbyssTechTree.java
+++ src/abyss/content/AbyssTechTree.java
@@ -100,6 +100,10 @@ public class AbyssTechTree implements ContentList {
 			node(AbyssBlocks.kaleniumConveyor, Seq.with(new Produce(AbyssItems.kalenium)));
 		});
 
+		extendNode(Blocks.router, () -> {
+			node(AbyssBlocks.kaleniumRouter, Seq.with(new Produce(AbyssItems.kalenium)));
+		});
+
 		extendNode(Blocks.plastaniumCompressor, () -> {
 			node(AbyssBlocks.abyssForge, Seq.with(new Produce(Items.thorium)));
 		});
