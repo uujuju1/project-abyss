629afc5ea0fa890cb45821c3336b39c221bd1e4e
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-31 11:03:58 -0300

    Add files via upload

---
+++ assets/sprites/blocks/force/ForceBlock.java
@@ -0,0 +1,58 @@
+package abyss.world.blocks.force;
+
+import arc.Core;
+import arc.util.*;
+import arc.graphics.*;
+import mindustry.ui.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.world.Block;
+import abyss.world.blocks.force.*;
+
+// block that can handle force
+public class ForceBlock extends Block{
+	public boolean outputsForce = true;
+	public float maxForce;
+	public Color forceColor = Pal.lancerLaser;
+
+	public ForceBlock(String name) {
+		super(name);
+		solid = update = true;
+	}
+
+	@Override
+	public void setBars() {
+		super.setBars();
+		bars.add("Force", entity -> new Bar(
+			Core.bundle.get("bar.force"),
+			forceColor,
+			() -> ((ForceBlockBuild) entity).forcef())
+		);
+	}
+
+	public class ForceBlockBuild extends Building implements ForceBlockc {
+		public ForceContainer force = new ForceContainer();
+
+		@Override
+		public ForceContainer forceContainer() {
+			return force;
+		}
+
+		@Override
+		public float forcef() {
+			return force.force/maxForce;
+		}
+
+		@Override
+		public void overloadForce() {
+			if (force.force > maxForce) {
+				kill();
+			}
+		}
+
+		@Override
+		public void updateTile() {
+			overloadForce();
+		}
+	}
+}
---
+++ assets/sprites/blocks/force/power/PullingBlock.java
@@ -0,0 +1,61 @@
+package abyss.world.blocks.force.power;
+
+import arc.Core;
+import arc.util.*;
+import arc.math.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.scene.ui.layout.*;
+import mindustry.gen.*;
+import abyss.world.blocks.force.*;
+
+public class PullingBlock extends ForceBlock {
+	public float pullMagnitude = 1f, releaseMagnitude = 0.2f;
+	public Color cableColorFrom, cableColorTo;
+	public TextureRegion bottom, cable, top;
+
+	public PullingBlock(String name) {
+		super(name);
+		maxForce = 100f;
+		configurable = true;
+		rotate = true;
+	}
+
+	@Override
+	public void load() {
+		super.load();
+		bottom = Core.atlas.find(name + "-bottom");
+		cable = Core.atlas.find(name + "-cable");
+		top = Core.atlas.find(name + "-top");
+	}
+
+	public class PullingBlockBuild extends ForceBlock.ForceBlockBuild {
+		boolean willPull = false;
+
+		@Override
+		public void buildConfiguration(Table table) {
+			table.button(Icon.settings, () -> {willPull = true;});
+		}
+
+		@Override
+		public void draw() {
+			Draw.rect(bottom, x, y, rotdeg());
+			Draw.color(cableColorFrom.lerp(cableColorTo, forcef()));
+			Draw.rect(cable, x, y, rotdeg());
+			Draw.color();
+			super.draw();
+			Draw.rect(top, x, y, rotdeg() + Math.min(forcef() * 400f, 360));
+		}
+
+		@Override
+		public void updateTile() {
+			super.updateTile();
+			if (willPull) {
+				setForce(Math.min(force.force + (Time.delta * pullMagnitude), maxForce), this);
+				if (force.force == maxForce) willPull = false;
+			} else {
+				setForce(Math.max(force.force - (Time.delta * releaseMagnitude), 0f), this);
+			}
+		}
+	}
+}
