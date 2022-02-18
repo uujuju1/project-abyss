c845d1898ef3e27a2abb999579fa996950eeae47
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-02-18 10:01:56 -0300

    Add files via upload

---
+++ src/abyss/world/blocks/defense/BaseItemTurret.java
@@ -0,0 +1,27 @@
+package abyss.world.blocks.defense;
+
+import arc.*;
+import arc.graphics.g2d.*;
+import mindustry.world.blocks.defense.turrets.*;
+// turret with custom rotating base
+public class BaseItemTurret extends ItemTurret {
+	public TextureRegion icon, wing;
+
+	public BaseItemTurret(String name) {
+		super.name();
+	}
+
+	@Override
+	public void load() {
+		icon = Core.atlas.find(name + "-icon");
+		wing = Core.atlas.find(name + "-wing");
+	}
+
+	public class BaseItemTurretBuild extends ItemTurret.ItemTurretBuild {
+		@Override
+		public void draw() {
+			Draw.rect(wing, x, y, rotation - 90);
+			super.draw();
+		}
+	}
+}
