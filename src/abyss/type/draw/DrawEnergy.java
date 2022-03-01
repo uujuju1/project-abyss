e002d6f1d30db53647cf26a270d95a7776dc54a5
uujuju1 <j4224253@gmail.com>
2022-02-28 21:28:39 -0300

    automated'ish commit

--- src/abyss/type/draw/DrawEnergy.java
+++ src/abyss/type/draw/DrawEnergy.java
@@ -1,5 +1,6 @@
 package abyss.type.draw;
 
+import arc.util.*;
 import arc.math.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
@@ -12,15 +13,16 @@ import mindustry.entities.units.*;
 public class DrawEnergy extends WeaponDrawer {
 	public Color[] colors = {Pal.heal, Color.white};
 	public float minRadius = 10f, mag = 1f;
+	float radius = 1;
 
 	@Override
 	public void draw(Unit unit, WeaponMount mount) {
 		radius = mount.reload * mount.weapon.reload;
 		Draw.color(colors[1]);
-		Fill.circle(mount.x, mount.y, (minRadius + (Mathf.sinDeg(Time.time) * mag)) * radius * unit.elevation);
+		Fill.circle(mount.weapon.x, mount.weapon.y, (minRadius + (Mathf.sinDeg(Time.time) * mag)) * radius * unit.elevation);
 
 		Draw.color(colors[2]);
-		Fill.circle(mount.x, mount.y, (minRadius/2 + (Mathf.sinDeg(Time.time) * mag)) * radius * unit.elevation);
+		Fill.circle(mount.weapon.x, mount.weapon.y, (minRadius/2 + (Mathf.sinDeg(Time.time) * mag)) * radius * unit.elevation);
 	} 
 
 	@Override
