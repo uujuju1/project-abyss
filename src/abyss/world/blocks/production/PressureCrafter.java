983c3482e4ebfd17fc4c448c5398fed1ce506b3e
uujuju1 <j4224253@gmail.com>
2022-03-04 20:21:20 -0300

    stuff

--- src/abyss/world/blocks/production/PressureCrafter.java
+++ src/abyss/world/blocks/production/PressureCrafter.java
@@ -6,9 +6,10 @@ import arc.util.io.*;
 import arc.graphics.*;
 import mindustry.ui.*;
 import mindustry.world.blocks.production.GenericCrafter;
+import mindustry.world.consumers.*;
 
 public class PressureCrafter extends GenericCrafter {
-	public float maxPressure = 350f, pressureBuildup = 0.001f, pressureThreshold = 300f;
+	public float maxPressure = 350f, pressureBuildup = 0.07f, pressureThreshold = 300f;
 
 	public PressureCrafter(String name) {
 		super(name);
@@ -18,7 +19,7 @@ public class PressureCrafter extends GenericCrafter {
 	public void setBars() {
 		super.setBars();
 		bars.add("BAR", entity -> new Bar(
-			Core.bundle.get("bar.pressure") + ((PressureCrafterBuild) entity).pressure(),
+			Core.bundle.get("bar.pressure"),
 			Color.white,
 			() -> ((PressureCrafterBuild) entity).pressuref()
 		));
@@ -30,66 +31,25 @@ public class PressureCrafter extends GenericCrafter {
 		public float pressuref() {
 			return pressure/maxPressure;
 		}
-		public float pressure() {
-			return pressure;
-		}
 
 		@Override
 		public void updateTile() {
-			if(consValid()){
+			if(cons.valid()) {
 				pressure = Mathf.approachDelta(pressure, maxPressure, pressureBuildup);
-
-				if(Mathf.chanceDelta(updateEffectChance)){
-					updateEffect.at(getX() + Mathf.range(size * 4f), getY() + Mathf.range(size * 4));
-				}
-
-				if (pressure >= pressureThreshold) {
-					progress += getProgressIncrease(craftTime);
-					totalProgress += delta();
-					warmup = Mathf.approachDelta(warmup, 1f, warmupSpeed);
-				}
-			}else{
-				pressure = Mathf.approachDelta(pressure, 0f, pressureBuildup);
-				warmup = Mathf.approachDelta(warmup, 0f, warmupSpeed);
-			}
-
-			if(progress >= 1f){
-				consume();
-
-				if(outputItem != null){
-					for(int i = 0; i < outputItem.amount; i++){
-						offload(outputItem.item);
-					}
-				}
-
-				if(outputLiquid != null){
-					handleLiquid(this, outputLiquid.liquid, outputLiquid.amount);
-				}
-
-				craftEffect.at(x, y);
-				progress %= 1f;
-			}
-
-			if(outputItem != null && timer(timerDump, dumpTime / timeScale)){
-				dump(outputItem.item);
-			}
-
-			if(outputLiquid != null){
-				dumpLiquid(outputLiquid.liquid);
+				super.updateTile();
 			}
 		}
 
 		@Override
 		public void write(Writes write){
-			super.write(write);
 			write.f(pressure);
+			super.write(write);
 		}
 
 		@Override
 		public void read(Reads read, byte revision){
-			super.read(read, revision);
 			pressure = read.f();
-			if(legacyReadWarmup) read.f();
+			super.read(read, revision);
 		}
 	}
 }
